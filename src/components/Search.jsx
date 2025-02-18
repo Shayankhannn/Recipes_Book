7d50042ab9d073906992f408b0eb1f28c8fbbb8e
shayankhannn <shayannkhannn007@gmail.com>
2025-02-18 17:29:09 +0500

    search trim

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,7 +7,9 @@ const Search = ({setSearchQuery}) => {
     <div  className="">
         <label  className="flex-hero gap-2 relative border border-black-200 rounded-full px-8 py-2  mx-4">
 
-      <input type="text" onChange={(e) => setSearchQuery(e.target.value)}  placeholder="Search By Title or Author" className="w-full h-[33px] rounded-md bg-transparent outline-none text-primary text-lg     "/>
+      <input type="text"
+       onChange={(e) => setSearchQuery(e.target.value.trim())} 
+        placeholder="Search By Title or Author..." className="w-full h-[33px] rounded-md bg-transparent outline-none text-primary text-lg     "/>
       <FaSearch className=" text-[20px] text-primary"/>
         </label>
     </div>
